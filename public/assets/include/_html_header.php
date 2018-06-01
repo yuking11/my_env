ddc67f6204b1722594611f40b76a7d005783e349
yuking <y.saito16@gmail.com>
2018-06-01 12:23:31 +0900

    metaタグ整理

--- public/assets/include/_html_header.php
+++ public/assets/include/_html_header.php
@@ -1,23 +1,30 @@
 <!DOCTYPE html>
 <html lang="ja" dir="ltr">
-<head>
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#">
 <meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<meta name="format-detection" content="telephone=no">
+<title><?php echo $title; ?></title>
 <?php if ($image_index!==0): ?><meta name="googlebot" content="noimageindex"><?php endif; ?>
 <?php if ($search_index!==0): ?><meta name="robots" content="noindex,nofollow"><?php endif; ?>
-<title><?php echo $title; ?></title>
+<?php // config ?>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width">
+<meta name="format-detection" content="telephone=no,email=no,address=no">
 <meta name="description" content="<?php echo $description; ?>">
 <meta name="keywords" content="<?php echo $keywords; ?>">
+<meta name="author" content="© 2018 Yuking11">
+<link rel="canonical" href="<?php echo CURRENT_URL; ?>">
+<?php // icons ?>
+<link href="<?php echo HTTP; ?>/assets/img/mask-icon.svg" rel="mask-icon" color="{{ カラーコード }}">
 <link href="<?php echo HTTP; ?>/assets/img/favicon.ico" rel="shortcut icon">
 <link href="<?php echo HTTP; ?>/assets/img/apple-touch-icon.png" rel="apple-touch-icon" type="image/png">
+<meta name="msapplication-config" content="/browserconfig.xml">
+<meta name="theme-color" content="{{ color }}">
 <?php // Facebook Open Graph ?>
 <meta property="fb:app_id" content="123456789">
 <meta property="og:url" content="<?php echo CURRENT_URL; ?>">
 <meta property="og:type" content="website">
 <meta property="og:title" content="<?php echo $title; ?>">
-<meta property="og:image" content="<?php echo HTTP; ?>/assets/img/og.jpg">
+<meta property="og:image" content="<?php echo HTTP; ?>/assets/img/ogp.jpg">
 <meta property="og:description" content="<?php echo $description; ?>">
 <meta property="og:site_name" content="<?php echo SITE_NAME; ?>">
 <meta property="og:locale" content="ja_JP">
