97455d07a7b9b9b8fe570495d54633dc865d2756
yuking <y.saito16@gmail.com>
2018-08-22 11:43:11 +0900

    webpack output path 修正

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   // ファイルの出力設定
   output: {
     //  出力ファイルのディレクトリ名
-    path: `${__dirname}/public_html/assets/js/`,
+    path: `${__dirname}/public/assets/js/`,
     // 出力ファイル名
     filename: 'app.js'
   },
