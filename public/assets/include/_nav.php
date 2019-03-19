bd13ad1b5997b9b6d754a7d122195f120f16ac24
yuking <ys11.works@gmail.com>
2019-03-19 11:45:07 +0900

    fixed Navigation Style

--- public/assets/include/_nav.php
+++ public/assets/include/_nav.php
@@ -21,6 +21,30 @@
       <li class="gnav_item"><a href="#" class="gnav_link">Menu4</a></li>
       <li class="gnav_item"><a href="#" class="gnav_link">Menu5</a></li>
       <li class="gnav_item"><a href="#" class="gnav_link">Menu6</a></li>
+      <li class="gnav_item"><a href="#" class="gnav_link">Menu7</a></li>
+      <li class="gnav_item"><a href="#" class="gnav_link">Menu8</a></li>
+      <li class="gnav_item"><a href="#" class="gnav_link">Menu9</a></li>
+      <li class="gnav_item"><a href="#" class="gnav_link">Menu10</a></li>
+      <li class="gnav_item"><a href="#" class="gnav_link">Menu11</a></li>
+      <li class="gnav_item gnav_item-dd">
+        <a href="#" class="gnav_link">Menu12</a>
+        <input type="checkbox" id="gnav_dd-12" name="gnav_dd-12" class="gnav_dd-ctrl">
+        <label class="gnav-sub_btn" for="gnav_dd-12"></label>
+        <ul class="gnav-sub">
+          <li class="gnav-sub_item"><a href="#" class="gnav_link">Menu12-1</a></li>
+          <li class="gnav-sub_item"><a href="#" class="gnav_link">Menu12-2</a></li>
+          <li class="gnav-sub_item"><a href="#" class="gnav_link">Menu12-3</a></li>
+          <li class="gnav-sub_item"><a href="#" class="gnav_link">Menu12-4</a></li>
+        </ul><!-- /.gnav-sub -->
+      </li>
+      <li class="gnav_item"><a href="#" class="gnav_link">Menu13</a></li>
+      <li class="gnav_item"><a href="#" class="gnav_link">Menu14</a></li>
+      <li class="gnav_item"><a href="#" class="gnav_link">Menu15</a></li>
+      <li class="gnav_item"><a href="#" class="gnav_link">Menu16</a></li>
+      <li class="gnav_item"><a href="#" class="gnav_link">Menu17</a></li>
+      <li class="gnav_item"><a href="#" class="gnav_link">Menu18</a></li>
+      <li class="gnav_item"><a href="#" class="gnav_link">Menu19</a></li>
+      <li class="gnav_item"><a href="#" class="gnav_link">Menu20</a></li>
     </ul><!-- /.gnav_list -->
   </nav><!-- /.gnav -->
 </div><!-- /.l-nav -->
